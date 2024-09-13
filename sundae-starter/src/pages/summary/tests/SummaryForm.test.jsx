ca3a338fbc060efa780350867f1957c3a266ef8e
Habib Daof <5386787+hdaof@users.noreply.github.com>
2024-09-12 18:36:12 -0700

    wrong files

--- sundae-starter/src/pages/summary/tests/SummaryForm.test.jsx
+++ sundae-starter/src/pages/summary/tests/SummaryForm.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
 import userEvent from "@testing-library/user-event";
 
@@ -22,30 +22,30 @@ test("Checkbox enables button on first click and disables on second click", asyn
   });
   const confirmButton = screen.getByRole("button", { name: /confirm order/i });
 
-  await user.click(checkbox);
+  fireEvent.click(checkbox);
   expect(confirmButton).toBeEnabled();
 
-  await user.click(checkbox);
+  fireEvent.click(checkbox);
   expect(confirmButton).toBeDisabled();
 });
 
-test("popover responds to hover", async () => {
-  const user = userEvent.setup();
-  render(<SummaryForm />);
+// test("popover responds to hover", async () => {
+//   const user = userEvent.setup();
+//   render(<SummaryForm />);
 
-  // popover starts out hidden
-  const nullPopover = screen.queryByText(
-    /no ice cream will actually be delivered/i
-  );
-  expect(nullPopover).not.toBeInTheDocument();
+//   // popover starts out hidden
+//   const nullPopover = screen.queryByText(
+//     /no ice cream will actually be delivered/i
+//   );
+//   expect(nullPopover).not.toBeInTheDocument();
 
-  // popover appears on mouseover of checkbox label
-  const termsAndConditions = screen.getByText(/terms and conditions/i);
-  await user.hover(termsAndConditions);
-  const popover = screen.getByText(/no ice cream will actually be delivered/i);
-  expect(popover).toBeInTheDocument();
+//   // popover appears on mouseover of checkbox label
+//   const termsAndConditions = screen.getByText(/terms and conditions/i);
+//   await user.hover(termsAndConditions);
+//   const popover = screen.getByText(/no ice cream will actually be delivered/i);
+//   expect(popover).toBeInTheDocument();
 
-  // popover disappears when we mouse out
-  await user.unhover(termsAndConditions);
-  expect(popover).not.toBeInTheDocument();
-});
+//   // popover disappears when we mouse out
+//   await user.unhover(termsAndConditions);
+//   expect(popover).not.toBeInTheDocument();
+// });
